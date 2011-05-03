b1434e3194e780b27098f4ac932c74b6df5876e4
Jan Krajdl <spm@spamik.cz>
2011-05-04 00:17:19 +0200

    Přejmenování, nastavení
    - přejmenován spouštěcí soubor kvůli kolizi s balíkem
    - importováno nastavení

--- src/tvt.py
+++ src/tvt.py
@@ -2,3 +2,10 @@
 # -*- coding: utf-8 -*-
 
 # TVTrack - nástroj pro automatické notifikace na nové seriály
+
+from tvtrack.settings import TVTrackSettings
+
+def main():
+    st = TVTrackSettings()
+
+main()
